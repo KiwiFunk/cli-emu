cff4fc3af5422f07f86512b8aad7b3922f844e7a
Alex Price <alex_p97@live.com>
2026-02-19 21:11:20 +0000

    Setup event listeners with xterm onData

--- src/components/terminal/Terminal.tsx
+++ src/components/terminal/Terminal.tsx
@@ -1,17 +1,40 @@
 import { Terminal as XTerm } from '@xterm/xterm';
 import { FitAddon } from '@xterm/addon-fit';
-import { useEffect } from 'react';
+import { useEffect, useRef } from 'react';
 
 function Terminal() {
+
+  const terminalRef = useRef<HTMLDivElement>(null);               // Ref to hold the terminal instance
 
   // Wait for DOM to be fully loaded before component mount.
   useEffect(() => {
     const terminal = new XTerm();                                 // Create new terminal instance
     const fitAddon = new FitAddon();                              // This allows the terminal to handle dynamic resizing
+    terminal.loadAddon(fitAddon);                                 // Load the fit addon into the terminal instance
 
     terminal.open(document.getElementById('terminal-display')!);  // Open terminal in the div container
     fitAddon.fit();                                               // Fit terminal to container size
     terminal.write('Hello, World!');                              // Test output
+
+    // xterm handles its own state, a persistant input buffer is fine
+    let inputBuffer = "";
+
+    // xterm has its own event system for handling user input
+    terminal.onData((data) => {
+      inputBuffer += data;                                        // Append new data to input buffer
+      if (data.includes('\r')) {                                  // Check for Enter key (carriage return)
+        terminal.write(`\r\nYou entered: ${inputBuffer.trim()}\r\n`);
+        inputBuffer = ""; // Clear the input buffer for the next command
+      }
+      if (data.includes('\u007F')) {                              // Check for Backspace key
+        inputBuffer = inputBuffer.slice(0, -1);                   // Remove the last character from the input buffer
+      }
+    });
+
+    // Cleanup function to dispose of terminal instance on unmount
+    return () => {
+      terminal.dispose();                                         // Terminal handles its own state and listeners.
+    }
   }, []);
 
   return (
