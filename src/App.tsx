fd76927c88147564fb024f48a37f7b10ec019d5f
Alex Price <alex_p97@live.com>
2026-02-20 17:47:21 +0000

    Created state to track fs init

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,20 @@
 // Component Imports
 import SplitView from './components/layout/SplitView'
 import initFs from './lib/initFs.ts';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 
 function App() {
 
+  const [fsReady, setFsReady] = useState(false);
+
   // Initialize the file system on component mount.
   useEffect(() => {
-      initFs();
+      initFs().then(() => setFsReady(true));
     }, [])
 
   return (
     <>
-      <SplitView className="p-4" />
+      <SplitView className="p-4" fsInit={fsReady} />
     </>
   )
 }
