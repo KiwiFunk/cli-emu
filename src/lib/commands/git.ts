8bbb784f299d2bce8f8c396b36b430c2a34e9a97
Alex Price <alex_p97@live.com>
2026-02-21 12:20:15 +0000

    Basic outline of needed commands in git.ts

--- src/lib/commands/git.ts
+++ src/lib/commands/git.ts
@@ -1,1 +1,18 @@
-// Git related commands (git init, git add, git commit etc.)
+import git from 'isomorphic-git';
+import fs from '../fileSystem';
+import { getCwd } from "../../store/useTerminalStore.ts";
+import type { CommandContext } from "../../types.ts";
+
+// git init
+await git.init({
+  fs,             // LightningFS instance
+  dir: getCwd()   // from Zustand store
+});
+
+// git add <file>
+
+// git commit -m "message"
+
+// git pull
+
+// git push
