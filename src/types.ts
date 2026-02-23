dc20c8b25f87011f903319e25707c72bc6b86103
Alex Price <alex_p97@live.com>
2026-02-23 15:57:00 +0000

    Updated CommandContext to make raw optional

--- src/types.ts
+++ src/types.ts
@@ -1,7 +1,7 @@
 export interface CommandContext {
   args: string[];                           // Positional arguments, e.g. ["file.txt", "dir/"]
   flags: Record<string, boolean | string>;  // Flags and options, e.g. { l: true, color: "red" }
-  raw: string;                              // The original raw input string for reference.
+  raw?: string | null;                      // The original raw input string for reference. (Optional)
 }
 
 export type CommandFn = (ctx: CommandContext) => Promise<string>;
