2fdf6a8c522e6bdf5540e2c9b5f77e5f90c5fe35
Alex Price <alex_p97@live.com>
2026-02-23 12:45:01 +0000

    Planned init and add funcs

--- src/lib/commands/git.ts
+++ src/lib/commands/git.ts
@@ -3,13 +3,45 @@ import fs from '../fileSystem';
 import { getCwd } from "../../store/useTerminalStore.ts";
 import type { CommandContext } from "../../types.ts";
 
+
+
+
+// Need to handle git init - create in getCwd
+// git init <directory> Create if doesnt exist.
 // git init
+export async function init(ctx: CommandContext): Promise<string> {
+  const { flags, args } = ctx;
+
+  // No directory specified, initialize in current working directory
+  if (args.length === 0) {
+
+  }
+
+  // CREATE 'EXISTS' function to check if directory exists, return bool
+  // if doesnt exist, attempt to create
+
+}
+
 await git.init({
   fs,             // LightningFS instance
   dir: getCwd()   // from Zustand store
 });
 
 // git add <file>
+export async function add(ctx: CommandContext): Promise<string> {
+
+  const { args } = ctx;
+
+  // Isomorphic-Git can't handle '.' by default, so walk directory and add each file individually
+  if (ctx.args == ".") {
+
+  }
+
+  try {
+    return "";
+  }
+
+};
 
 // git commit -m "message"
 
