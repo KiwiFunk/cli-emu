1bc11823c5d787161961e5c0ebb5579b30ce5927
Alex Price <alex_p97@live.com>
2026-03-03 16:20:12 +0000

    Added buffer dependency and init in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,3 +1,7 @@
+// Buffer is not provided by Vite, but is expected by isomorphic-git.
+import { Buffer } from 'buffer';
+(window as unknown as { Buffer: typeof Buffer }).Buffer = Buffer;
+
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
