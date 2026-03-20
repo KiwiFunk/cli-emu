e5f89b6b2c696f6ee738413ba871bd665a6de832
Alex Price <alex_p97@live.com>
2026-03-20 11:36:56 +0000

    Cleaned up spelling mistake

--- src/store/useRepoStore.ts
+++ src/store/useRepoStore.ts
@@ -1,5 +1,5 @@
 // This store handles states related to the repository emulation.
-// Although both the 'remote' and 'local' directories share the same FS, we treat them as separate entiries.
+// Although both the 'remote' and 'local' directories share the same FS, we treat them as separate entities.
 // As such, we use a unique Zustand store for both.
 
 import { create } from 'zustand';
