43802129ebee706a946d32fc6f8f11f219c05d72
Alex Price <alex_p97@live.com>
2026-05-03 09:18:24 +0100

    Overhaul header of indexview

--- src/components/browser/providers/Azure/views/IndexView.tsx
+++ src/components/browser/providers/Azure/views/IndexView.tsx
@@ -1,4 +1,4 @@
-import { Book, Plus } from "lucide-react";
+import { Book, Plus, Funnel } from "lucide-react";
 import { useRemoteIndex } from "@/hooks/useRemoteIndex";
 import type { RepoIndexProps } from "@/types";
 
@@ -8,16 +8,48 @@ export default function AzureIndexView({ onSelectRepo, onNewRepo }: RepoIndexPro
   const getDisplayName = (path: string) => path.split("/").pop()?.replace(".git", "") || path;
 
   return (
-    <div className="p-8 max-w-6xl mx-auto">
-      <div className="flex justify-between items-center mb-6">
-        <h1 className="text-2xl font-semibold text-white">Repositories</h1>
-        <button
-          onClick={onNewRepo}
-          className="bg-blue-600 hover:bg-blue-500 text-white px-4 py-1.5 rounded text-sm font-semibold flex items-center gap-2"
-        >
-          <Plus size={16} /> New repository
-        </button>
+    <div className="p-8 w-full max-w-6xl mx-auto">
+
+      {/* Section Header */}
+      <div className="w-full flex-row gap-3 items-center mb-6">
+
+        {/* Org Name / CTA Buttons */}
+        <div className="flex justify-between items-center gap-4 w-full">
+          {/* Organization Name - Hardcode for now as orgs are not in scope */}
+          <h1 className="text-2xl font-semibold text-neutral-800">gitsim</h1>
+          <button
+            onClick={onNewRepo}
+            className="bg-sky-600 hover:bg-sky-700 text-white cursor-pointer px-3 py-1.75 rounded-xs text-sm font-semibold flex items-center gap-3"
+          >
+            <Plus size={20} /> New project
+          </button>
+        </div>
+
+        {/* Tabs and Filters */}
+        <div className="flex justify-between items-center gap-4 w-full pt-4">
+
+          <div className="flex items-center gap-4">
+            <button className="py-2 text-sm text-gray-800 hover:text-gray-800 transition-colors font-semibold border-b-2 border-sky-500 ">
+              Projects
+            </button>
+            <button className="py-2 text-sm text-gray-600 hover:text-gray-800 transition-colors">
+              My work items
+            </button>
+            <button className="py-2 text-sm text-gray-600 hover:text-gray-800 transition-colors">
+              My pull requests
+            </button>
+          </div>
+
+          <div className="flex bg-neutral-200 items-center gap-2 px-2 py-1 text-sm text-gray-500 w-64 transition-colors">
+            <Funnel size={16} className="text-gray-500" />
+            <span className="flex-1 outline-none text-gray-600 bg-transparent">
+              Filter projects
+            </span>
+          </div>
+
+        </div>
       </div>
+
 
       <div className="bg-[#111018] border border-gray-800 rounded-md shadow-sm">
         {loading ? (
